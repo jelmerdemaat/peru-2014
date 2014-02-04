8cd9d252e31e44d663a5b1dabadfcd5bb8d9d371
Jelmer de Maat <post@jelmerdemaat.nl>
2014-02-04 17:42:10 +0100

    responsive container css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,40 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
+	require('load-grunt-tasks')(grunt);
 
-    pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		options: {
+			'no-beep': true
+		},
+		sass: {
+			dist: {
+				options: {
+					style: 'compact',
+					compass: true
+				},
+				files: {
+					'css/peru.css': 'sass/peru.sass',
+				}
+			}
+		},
+		jekyll: {                             // Task
+			options: {                          // Universal options
+			},
+			dist: {                             // Target
+				options: {                        // Target options
+				}
+			}
+		},
+		watch: {
+			options: {
+				livereload: true
+			},
+			files: ['sass/**/*.sass','sass/**/*.scss','**/*.htm'],
+			tasks: 'sass'
+		}
+	});
 
-    options: {
-      'no-beep': true
-    },
-
-    // uglify: {
-    //   options: {
-    //     banner: '/*! <%= pkg.name %> | <%= grunt.template.today("dd-mm-yyyy - HH:mm:ss") %> */\n'
-    //   },
-    //   dist: {
-    //     files: {
-    //       'js/peru.min.js': ['js/peru.js']
-    //     }
-    //   }
-    // },
-
-    sass: {
-        dist: {
-          options: {
-            style: 'compact',
-            compass: true
-          },
-          files: {
-            'css/peru.css': 'sass/peru.sass',
-          }
-        }
-      },
-
-    watch: {
-    	options: {
-        livereload: true
-    	},
-  		files: ['sass/**/*.sass','sass/**/*.scss'],
-  		tasks: 'sass'
-    }
-
-  });
-
-
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerTask('default', ['sass']);
+	grunt.registerTask('default', ['sass','jekyll']);
 
 };
